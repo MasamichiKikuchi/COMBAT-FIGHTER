0aa4d49714cad1870a48c3f47f67024fd66ddb9e
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-11-27 18:58:58 +0900

    ミサイルが一定距離進んだら消滅する

--- Assets/MissileController.cs
+++ Assets/MissileController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MissileController : MonoBehaviour
@@ -7,24 +8,33 @@ public class MissileController : MonoBehaviour
     public Transform target; // ロックオンした敵
     public float speed = 5f; // ミサイルの速度
 
+    public float maxDistance = 10f; // 一定距離
+
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        // ゲームオブジェクトの初期位置を保存
+        initialPosition = transform.position;
+    }
+
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
+        float distance = Vector3.Distance(initialPosition, transform.position);
+    
+       if(maxDistance >= distance )
+       {
+            // 目標の方向を向く
+            transform.LookAt(target);
 
-            if (target != null)
-            {
-                // 目標の方向を向く
-                transform.LookAt(target);
-
-                // ミサイルの移動
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
-            }
-            else
-            {
-                // ターゲットがない場合、ミサイルを破壊などの処理を追加
-                Destroy(gameObject);
-            }
+            // ミサイルの移動
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
+
+        else 
+        { 
+            Destroy(gameObject); 
+        }
+
     }
 }
