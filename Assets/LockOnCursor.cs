98b56164ed2d77d4fd929172872fba0ba4ca8664
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-11-30 19:20:02 +0900

    ロックオンカーソルを作成
    敵をロックオンするとカーソルが動く。
    ロックオンしていない場合はデフォルト位置に固定。

---
+++ Assets/LockOnCursor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class LockOnCursor : MonoBehaviour
+    {
+        [SerializeField] private RectTransform parentRectTransform;
+        [SerializeField] private Transform playerTransform;
+        [SerializeField] public GameObject lockedEnemy;
+   
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+        {
+         UpdateLifeGaugePosition();
+         Debug.Log($"{lockedEnemy}");
+        
+        }
+
+        private void UpdateLifeGaugePosition()
+        {
+           
+        if (lockedEnemy != null)
+        {
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(lockedEnemy.transform.position);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
+            transform.localPosition = localPoint;
+        }
+        else
+        {
+            Vector3 screenPoint = Camera.main.WorldToScreenPoint(playerTransform.position);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
+            transform.localPosition = localPoint + new Vector2(0, 50);
+        }
+        }
+    }
