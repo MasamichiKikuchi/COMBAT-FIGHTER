bf344bd903670ea6b8bebe759b095a3511f7abfa
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-12-01 19:23:43 +0900

    プレイヤーのダメージ機能を実装
    敵の攻撃が当たったらダメージをくらう。

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int hp;
+    int maxHp = 10;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHp;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Damage(int damage) 
+    {
+        hp -= damage;
+        Debug.Log($"プレイヤーのHP:{hp}");
+        if (hp <= 0) 
+        {
+            Debug.Log("ゲームオーバー");        
+        }
     }
 }
