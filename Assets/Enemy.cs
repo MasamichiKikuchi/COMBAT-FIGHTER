b090661d27429d5800e4276b74f778bcb9d49dcf
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-12-02 19:36:46 +0900

    敵がランダムな方向に回避する

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+    protected enum StateEnum
+    {
+        Normal,
+        Attack,
+        Avoid,
+    }
+
+    bool Attack => StateEnum.Attack == state;
+    bool Avoid => StateEnum.Avoid == state;
+
+    protected StateEnum state = StateEnum.Normal;
+
     int hp;
     int maxHp =1;
    
     public BoxCollider enemyEscapeArea; //プレイヤーを感知したら回避行動をとる範囲のボックスコライダーコンポーネント
     public float avoidanceSpeed = 200f;    // 回避行動時の速度
     private Vector3 avoidanceDirection; // 回避行動の方向
-    
+    public float rotateSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,14 +47,24 @@ public class Enemy : MonoBehaviour
           FireController.enemies.Remove(gameObject);     
         }
     }
-    public void AvoidMove(Collider collider)
-    {          
-           // 回避行動の方向を計算
-            avoidanceDirection = transform.position - collider.transform.position;
-            avoidanceDirection.y = 0f; // Y軸方向の変化を無視
-      
-        // 回避行動の方向に向かって移動
-        transform.Translate(avoidanceDirection.normalized * avoidanceSpeed * Time.deltaTime);
+
+    public Vector3 AvoidDirection()
+    {
+        // ランダムな方向に回避行動
+        Vector3 randomDirection = Random.insideUnitSphere;
+
+        return randomDirection;
+
+    }
+
+    public void AvoidMove(Collider collider,Vector3 vector3)
+    {
+        
+        vector3.z = 0f; // Z軸方向の変化を無視
+
+        Quaternion targetRotation = Quaternion.LookRotation(vector3);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * avoidanceSpeed * Time.deltaTime);
     }
 
 
