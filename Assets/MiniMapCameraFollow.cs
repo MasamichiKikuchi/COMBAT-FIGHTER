e722fc2f95e5ec593e0eee11c862133735a83890
MasamichiKikuchi2023 <136216247+MasamichiKikuchi2023@users.noreply.github.com>
2023-12-05 17:21:27 +0900

    ミニマップがプレイヤーの向きに追随する

--- Assets/MiniMapCameraFollow.cs
+++ Assets/MiniMapCameraFollow.cs
@@ -10,6 +10,10 @@ public class MiniMapCameraFollow : MonoBehaviour
     void LateUpdate()
     {
         // プレイヤーの位置に追随する
-        transform.position = new Vector3(playerTransform.position.x, heightAbovePlayer, playerTransform.position.z);
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y + heightAbovePlayer, playerTransform.position.z);
+        // プレイヤーの向きに応じてミニマップカメラを回転させる
+        float playerRotation = playerTransform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Euler(90f, playerRotation, 0f);
     }
+
 }
