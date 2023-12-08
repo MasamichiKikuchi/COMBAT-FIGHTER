d14e1be4dc4faffcdaf8c23315ce01f9b673d932
MasamichiKikuchi2023 <uni202306_05@telework.to>
2023-12-08 21:14:07 +0900

    ロックオンカーソルの色を変える実験

--- Assets/LockOnCursor.cs
+++ Assets/LockOnCursor.cs
@@ -1,38 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using TMPro;
 using UnityEngine;
 
     public class LockOnCursor : MonoBehaviour
     {
-        [SerializeField] private RectTransform parentRectTransform;
-        [SerializeField] private Transform playerTransform;
-        [SerializeField] public GameObject lockedEnemy;
+        public RectTransform parentRectTransform;
+        public Transform playerTransform;
+        public GameObject lockedEnemy;
+        public TextMeshProUGUI meshPro;
    
-
-    private void Start()
+    void Start ()
     {
-        
+        //meshPro = gameObject.GetComponent<TextMeshPro>();
+        meshPro.text = "<color=yellow>[ ] </color>";
     }
 
     private void Update()
-        {
-         UpdateLifeGaugePosition();        
-        }
-
-        private void UpdateLifeGaugePosition()
-        {
-           
-        if (lockedEnemy != null)
-        {
-            Vector3 screenPoint = Camera.main.WorldToScreenPoint(lockedEnemy.transform.position);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
-            transform.localPosition = localPoint;
-        }
-        else
-        {
-            Vector3 screenPoint = Camera.main.WorldToScreenPoint(playerTransform.position);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
-            transform.localPosition = localPoint + new Vector2(0,80);
-        }
-        }
+    {
+      UpdateLockOnCursorPosition(); 
+        
     }
+
+    private void UpdateLockOnCursorPosition()
+    {         
+       if (lockedEnemy != null)
+        {
+            string text = " [ ]";
+            meshPro.text = $"<color=red>{text} </color>";
+           Vector3 screenPoint = Camera.main.WorldToScreenPoint(lockedEnemy.transform.position);
+           RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
+           transform.localPosition = localPoint;
+       }
+       
+       else
+       {
+           meshPro.text = "<color=green>[ ]</color><color=red>A</color>";
+           Vector3 screenPoint = Camera.main.WorldToScreenPoint(playerTransform.position);
+           RectTransformUtility.ScreenPointToLocalPointInRectangle(parentRectTransform, screenPoint, null, out Vector2 localPoint);
+           transform.localPosition = localPoint + new Vector2(0,100);
+       }
+    }
+}
