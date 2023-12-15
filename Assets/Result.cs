fcc0b4b45fbb13f5315d464c031ee5c8fd724511
MasamichiKikuchi2023 <uni202306_05@telework.to>
2023-12-15 17:31:11 +0900

    ランキングが出ない

--- Assets/Result.cs
+++ Assets/Result.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ResultManager : MonoBehaviour
+public class Result : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI lifeBonusText;
